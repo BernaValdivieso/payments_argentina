35c5a36bb1dc6ef5dc6ca3b49daf80782de58261
Bernardo Valdivieso <bernardo@Bernardos-MacBook-Air.local>
2025-05-22 13:34:34 -0400

    Move wsgi.py to payment_processor directory and update paths

--- payment_processor/wsgi.py
+++ payment_processor/wsgi.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # Add the project root directory to the Python path
-current_dir = os.path.dirname(os.path.abspath(__file__))
+current_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, current_dir)
 
 from payment_processor.app import create_app
